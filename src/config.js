c1ce16fa257188c2296023ea9a33bf008bca3b3f
Alim <alim.askerov@gmail.com>
2026-05-21 20:06:26 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,18 +3,18 @@ export const LESSON_TIMES = [
   { pair: 2, start: '10:05', end: '11:30' },
   { pair: 3, start: '12:00', end: '13:25' },
   { pair: 4, start: '13:35', end: '15:00' },
-  { pair: 5, start: '15:10', end: '16:35' },
-  { pair: 6, start: '16:45', end: '18:10' },
-  { pair: 7, start: '18:20', end: '19:45' },
+  { pair: 5, start: '15:30', end: '16:55' },
+  { pair: 6, start: '17:05', end: '18:30' },
+  { pair: 7, start: '18:40', end: '20:05' },
 ];
 
 export const BREAK_TIMES = [
   { after: 1, start: '09:55', end: '10:05', duration: 10 },
   { after: 2, start: '11:30', end: '12:00', duration: 30 },
   { after: 3, start: '13:25', end: '13:35', duration: 10 },
-  { after: 4, start: '15:00', end: '15:10', duration: 10 },
-  { after: 5, start: '16:35', end: '16:45', duration: 10 },
-  { after: 6, start: '18:10', end: '18:20', duration: 10 },
+  { after: 4, start: '15:00', end: '15:30', duration: 30 },
+  { after: 5, start: '16:55', end: '17:05', duration: 10 },
+  { after: 6, start: '18:30', end: '18:40', duration: 10 },
 ];
 
 export const WEEKDAYS_RU = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
